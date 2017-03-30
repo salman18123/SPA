526c68359fda12f0ec392ca87e0462c04c9921e8
salman ahmed <salmanvit12345@gmail.com>
2017-03-30 11:00:14 +0530

    SPA

---
+++ jquery.js
@@ -0,0 +1,189 @@
+$(document).ready(function(){
+	$("#sub").hide();
+
+$("#manutd").on('click',function(){
+	find('manutd');
+	document.write('total matches played are'+' '+team.matchesPlayed +'<br>');
+	document.write('total goals are'+' '+team.sumOfGoals+'<br>');
+	document.write('total losses'+' '+team.totalLosses +'<br>');
+	document.write('total wins'+' '+team.totalWins +'<br>');
+	document.write('total Draw'+' '+team.totalDraw +'<br>');
+	$("#1").remove();
+
+	 
+});
+$("#sub").hide();
+
+$("#tottenham").on('click',function(){
+	find('tottenham');
+	 document.write('total matches played are'+' '+team.matchesPlayed +'<br>');
+	document.write('total goals are'+' '+team.sumOfGoals+'<br>');
+	document.write('total losses'+' '+team.totalLosses +'<br>');
+	document.write('total wins'+' '+team.totalWins +'<br>');
+	document.write('total Draw'+' '+team.totalDraw +'<br>');
+	$("#1").remove();
+});
+$("#bournemouth").on('click',function(){
+	find('bournemouth');
+	document.write('total matches played are'+' '+team.matchesPlayed +'<br>');
+	document.write('total goals are'+' '+team.sumOfGoals+'<br>');
+	document.write('total losses'+' '+team.totalLosses +'<br>');
+	document.write('total wins'+' '+team.totalWins +'<br>');
+	document.write('total Draw'+' '+team.totalDraw +'<br>');
+	$("#1").remove();
+});
+$("#astonvilla").on('click',function(){
+	find('astonvilla');
+	document.write('total matches played are'+' '+team.matchesPlayed +'<br>');
+	document.write('total goals are'+' '+team.sumOfGoals+'<br>');
+	document.write('total losses'+' '+team.totalLosses +'<br>');
+	document.write('total wins'+' '+team.totalWins +'<br>');
+	document.write('total Draw'+' '+team.totalDraw +'<br>');
+	$("#1").remove();
+});
+$("#everton").on('click',function(){
+	find('everton');
+	document.write('total matches played are'+' '+team.matchesPlayed +'<br>');
+	document.write('total goals are'+' '+team.sumOfGoals+'<br>');
+	document.write('total losses'+' '+team.totalLosses +'<br>');
+	document.write('total wins'+' '+team.totalWins +'<br>');
+	document.write('total Draw'+' '+team.totalDraw +'<br>');
+	$("#1").remove();
+  });
+$("#watford").on('click',function(){
+	find('watford');
+	document.write('total matches played are'+' '+team.matchesPlayed +'<br>');
+	document.write('total goals are'+' '+team.sumOfGoals+'<br>');
+	document.write('total losses'+' '+team.totalLosses +'<br>');
+	document.write('total wins'+' '+team.totalWins +'<br>');
+	document.write('total Draw'+' '+team.totalDraw +'<br>');
+	$("#1").remove();
+});
+$("#leicester").on('click',function(){
+	find('leicester');
+	document.write('total matches played are'+' '+team.matchesPlayed +'<br>');
+	document.write('total goals are'+' '+team.sumOfGoals+'<br>');
+	document.write('total losses'+' '+team.totalLosses +'<br>');
+	document.write('total wins'+' '+team.totalWins +'<br>');
+	document.write('total Draw'+' '+team.totalDraw +'<br>');
+	$("#1").remove();
+});
+$("#sunderland").on('click',function(){
+	find('sunderland');
+	document.write('total matches played are'+' '+team.matchesPlayed +'<br>');
+	document.write('total goals are'+' '+team.sumOfGoals+'<br>');
+	document.write('total losses'+' '+team.totalLosses +'<br>');
+	document.write('total wins'+' '+team.totalWins +'<br>');
+	document.write('total Draw'+' '+team.totalDraw +'<br>');
+	$("#1").remove();
+});
+$("#norwich").on('click',function(){
+	find('norwich');
+	document.write('total matches played are'+' '+team.matchesPlayed +'<br>');
+	document.write('total goals are'+' '+team.sumOfGoals+'<br>');
+	document.write('total losses'+' '+team.totalLosses +'<br>');
+	document.write('total wins'+' '+team.totalWins +'<br>');
+	document.write('total Draw'+' '+team.totalDraw +'<br>');
+	$("#1").remove();
+});
+$("#crystslpalace").on('click',function(){
+	find('crystalpalace');
+	document.write('total matches played are'+' '+team.matchesPlayed +'<br>');
+	document.write('total goals are'+' '+team.sumOfGoals+'<br>');
+	document.write('total losses'+' '+team.totalLosses +'<br>');
+	document.write('total wins'+' '+team.totalWins +'<br>');
+	document.write('total Draw'+' '+team.totalDraw +'<br>');
+	$("#1").remove();
+});
+$("#chelsea").on('click',function(){
+	find('chelsea');
+	document.write('total matches played are'+' '+team.matchesPlayed +'<br>');
+	document.write('total goals are'+' '+team.sumOfGoals+'<br>');
+	document.write('total losses'+' '+team.totalLosses +'<br>');
+	document.write('total wins'+' '+team.totalWins +'<br>');
+	document.write('total Draw'+' '+team.totalDraw +'<br>');
+	$("#1").remove();
+});
+$("#swansea").on('click',function(){
+	find('swansea');
+	document.write('total matches played are'+' '+team.matchesPlayed +'<br>');
+	document.write('total goals are'+' '+team.sumOfGoals+'<br>');
+	document.write('total losses'+' '+team.totalLosses +'<br>');
+	document.write('total wins'+' '+team.totalWins +'<br>');
+	document.write('total Draw'+' '+team.totalDraw +'<br>');
+	$("#1").remove();
+});
+$("#arsenal").on('click',function(){
+	find('arsenal');
+	document.write('total matches played are'+' '+team.matchesPlayed +'<br>');
+	document.write('total goals are'+' '+team.sumOfGoals+'<br>');
+	document.write('total losses'+' '+team.totalLosses +'<br>');
+	document.write('total wins'+' '+team.totalWins +'<br>');
+	document.write('total Draw'+' '+team.totalDraw +'<br>');
+	$("#1").remove();
+});
+$("#westham").on('click',function(){
+	find('westham');
+	document.write('total matches played are'+' '+team.matchesPlayed +'<br>');
+	document.write('total goals are'+' '+team.sumOfGoals+'<br>');
+	document.write('total losses'+' '+team.totalLosses +'<br>');
+	document.write('total wins'+' '+team.totalWins +'<br>');
+	document.write('total Draw'+' '+team.totalDraw +'<br>');
+	$("#1").remove();
+});
+$("#newcastle").on('click',function(){
+	find('newcastle');
+	document.write('total matches played are'+' '+team.matchesPlayed +'<br>');
+	document.write('total goals are'+' '+team.sumOfGoals+'<br>');
+	document.write('total losses'+' '+team.totalLosses +'<br>');
+	document.write('total wins'+' '+team.totalWins +'<br>');
+	document.write('total Draw'+' '+team.totalDraw +'<br>');
+	$("#1").remove();
+});
+$("#southampton").on('click',function(){
+	find('southampton');
+	document.write('total matches played are'+' '+team.matchesPlayed +'<br>');
+	document.write('total goals are'+' '+team.sumOfGoals+'<br>');
+	document.write('total losses'+' '+team.totalLosses +'<br>');
+	document.write('total wins'+' '+team.totalWins +'<br>');
+	document.write('total Draw'+' '+team.totalDraw +'<br>');
+	$("#1").remove();
+});
+$("#stoke").on('click',function(){
+	find('stoke');
+	document.write('total matches played are'+' '+team.matchesPlayed +'<br>');
+	document.write('total goals are'+' '+team.sumOfGoals+'<br>');
+	document.write('total losses'+' '+team.totalLosses +'<br>');
+	document.write('total wins'+' '+team.totalWins +'<br>');
+	document.write('total Draw'+' '+team.totalDraw +'<br>');
+	$("#1").remove();
+});
+$("#liverpool").on('click',function(){
+	find('liverpool');
+	document.write('total matches played are'+' '+team.matchesPlayed +'<br>');
+	document.write('total goals are'+' '+team.sumOfGoals+'<br>');
+	document.write('total losses'+' '+team.totalLosses +'<br>');
+	document.write('total wins'+' '+team.totalWins +'<br>');
+	document.write('total Draw'+' '+team.totalDraw +'<br>');
+	$("#1").remove();
+});
+$("#westbrom").on('click',function(){
+	find('westbrom');
+	document.write('total matches played are'+' '+team.matchesPlayed +'<br>');
+	document.write('total goals are'+' '+team.sumOfGoals+'<br>');
+	document.write('total losses'+' '+team.totalLosses +'<br>');
+	document.write('total wins'+' '+team.totalWins +'<br>');
+	document.write('total Draw'+' '+team.totalDraw +'<br>');
+	$("#1").remove();
+});
+$("#mancity").on('click',function(){
+	find('mancity');
+	document.write('total matches played are'+' '+team.matchesPlayed +'<br>');
+	document.write('total goals are'+' '+team.sumOfGoals+'<br>');
+	document.write('total losses'+' '+team.totalLosses +'<br>');
+	document.write('total wins'+' '+team.totalWins +'<br>');
+	document.write('total Draw'+' '+team.totalDraw +'<br>');
+	$("#1").remove();
+});
+
+});
